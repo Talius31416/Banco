c018ce81bad36db008014c088e004f8ab0115c24
Talius31416 <tomixplayer07@gmail.com>
2025-05-06 09:46:04 -0500

    Update Empresarial.java

--- ProyectoFinal/src/main/java/Java/Intermedio/model/Empresarial.java
+++ ProyectoFinal/src/main/java/Java/Intermedio/model/Empresarial.java
@@ -1,7 +1,31 @@
 package Java.Intermedio.model;
 
 public class Empresarial implements CuentaBancaria{
-    private int montoEmpleados;
-    private int montoEmpresa;
+    private float montoEmpleados;
+    private float montoEmpresa;
     private Cliente cliente;
+    public void setMontoEmpleados(int montoEmpleados) {
+        this.montoEmpleados = montoEmpleados;
+    }
+    public void setMontoEmpresa(float montoEmpresa) {
+        this.montoEmpresa = montoEmpresa;
+    }
+    public float getMontoEmpleados() {
+        return montoEmpleados;
+    }
+    public float getMontoEmpresa() {
+        return montoEmpresa;
+    }
+    @Override
+    public void depositar(float monto) {
+        this.montoEmpresa += monto;
+    }
+    @Override
+    public boolean retirar(float monto) {
+        if(this.montoEmpresa >= monto){
+            this.montoEmpresa -= monto;
+            return true;
+        }
+        return false;
+    }
 }
