2d1cd196c087df3304ece86854182f7bdc37634c
Talius31416 <tomixplayer07@gmail.com>
2025-05-11 16:09:28 -0500

    Update Corriente.java

--- ProyectoFinal/src/main/java/Java/Intermedio/model/Corriente.java
+++ ProyectoFinal/src/main/java/Java/Intermedio/model/Corriente.java
@@ -1,8 +1,25 @@
 package Java.Intermedio.model;
 
 public class Corriente implements CuentaBancaria{
-    private int monto;
+    private float monto;
     private Cliente cliente;
     private boolean tarjetaCredito;
-
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+    public float getMonto() {
+        return monto;
+    }
+    @Override
+    public void depositar(float monto) {
+        this.monto += monto;
+    }
+    @Override
+    public boolean retirar(float monto) {
+        if(this.monto >= monto || (tarjetaCredito && this.monto >= monto-1000000)) {
+            this.monto -= monto;
+            return true;
+        }
+        return false;
+    }
 }
