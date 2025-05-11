89b8ad11f52141a7ca3a692bfcce2a35f0213c84
Talius31416 <tomixplayer07@gmail.com>
2025-05-11 16:09:03 -0500

    Update Ahorro.java

--- ProyectoFinal/src/main/java/Java/Intermedio/model/Ahorro.java
+++ ProyectoFinal/src/main/java/Java/Intermedio/model/Ahorro.java
@@ -1,7 +1,32 @@
 package Java.Intermedio.model;
 
 public class Ahorro implements CuentaBancaria{
-    private int monto;
-    private int interesesPorMes;
+    private float monto;
+    private float interesesPorMes;
     private Cliente cliente;
+     public void setMonto(int monto) {
+         this.monto = monto;
+     }
+     public void setInteresesPorMes(float interesesPorMes) {
+         this.interesesPorMes = interesesPorMes;
+     }
+     public float getMonto() {
+         return monto;
+     }
+     public float getInteresesPorMes() {
+         return interesesPorMes;
+     }
+
+    @Override
+    public void depositar(float monto) {
+         this.monto += monto;
+    }
+    @Override
+    public boolean retirar(float monto) {
+         if(this.monto >= monto){
+             this.monto -= monto;
+             return true;
+         }
+         return false;
+    }
 }
